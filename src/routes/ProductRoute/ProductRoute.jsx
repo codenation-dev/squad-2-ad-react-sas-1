03ff4b5610916be06bbf403f41a7e6b0c424a864
thierry rene matos <td_matos@terra.com.br>
2020-05-17 17:12:47 -0300

    update - add new files from product page

---
+++ src/routes/ProductRoute/ProductRoute.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+
+import Product from "../../containers/Product";
+
+import "./ProductRoute.scss";
+
+const ProductRoute = () => {
+	return <Product />
+};
