d56bb8cd9c7d20ae3b6c0afd96713957a1a19bda
thierry rene matos <td_matos@terra.com.br>
2020-05-17 17:31:33 -0300

    update'

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-<<<<<<< HEAD
 import ProductListRoute from './ProductListRoute';
 import ProductRoute from './ProductRoute';
 import Search from '../components/Search/Search';
-=======
-import ProductListRoute from "./ProductListRoute";
-//import ProductPageRoute from "./ProductPageRoute";
-import Search from "../components/Search/Search";
->>>>>>> 87cbccaed9a2ce30e669f736d3c341ea61f93e95
 
 const Routes = () => (
   <Switch>
