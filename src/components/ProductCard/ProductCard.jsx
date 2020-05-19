cc772bdabd120d2b12fafed071e565aed8e04d6d
unknown <paloma.s@live.com>
2020-05-18 21:48:31 -0300

    update component in process

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,19 +1,18 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { useHistory  } from "react-router-dom"
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import Indisponivel from '../../assets/images/indisponivel.png';
 
 import ProductDetail from '../ProductDetail';
 
-import "./ProductCard.scss";
+import './ProductCard.scss';
 
-const ProductCard = ( {productInfo} ) => {
-
-  const history = useHistory()
+const ProductCard = ({ productInfo }) => {
+  const history = useHistory();
 
   const clickHandle = () => {
-
-    history.push(`/products/${productInfo.name}`,{ productInfo })
-  }
+    history.push(`/products/${productInfo.name}`, { productInfo });
+  };
   // const {
   //   name,
   //   discount_percentage,
@@ -27,23 +26,28 @@ const ProductCard = ( {productInfo} ) => {
   //   const slug = `/product-detail-${name}`
   // }
 
-  console.log('card', productInfo);
+  // console.log('card', productInfo);
+  console.log({ Indisponivel });
 
   return (
     <div className="product__card__container">
       <div className="product__card__grid">
         <div className="product__card__image">
-          <Link to={`/products/${productInfo.name}`} >
-            <img className="pic__1" src={productInfo.image} alt={productInfo.name} />
+          <Link to={`/products/${productInfo.name}`}>
+            <img
+              className="pic__1"
+              src={productInfo.image === '' ? Indisponivel : productInfo.image}
+              alt={productInfo.name}
+            />
           </Link>
           <span className="product__card__trend__label">Promoção</span>
           <span className="product__card__discount__label">
             {productInfo.discount_percentage}
           </span>
           <ul className="social">
-            <li onClick={clickHandle}>
-                Detalhes
-                <ion-icon name="cart"/>
+            <li>
+              <h3>Detalhes</h3>
+              <ion-icon name="cart" onClick={clickHandle} />
             </li>
           </ul>
         </div>
@@ -52,7 +56,9 @@ const ProductCard = ( {productInfo} ) => {
             <Link to={`/products/${productInfo.name}`}>{productInfo.name}</Link>
           </h3>
           <div className="price discount">
-            <span>{productInfo.regular_price}</span>
+            {productInfo.regular_price == productInfo.actual_price ? null : (
+              <span>{productInfo.regular_price}</span>
+            )}
             {productInfo.actual_price}
           </div>
         </div>
